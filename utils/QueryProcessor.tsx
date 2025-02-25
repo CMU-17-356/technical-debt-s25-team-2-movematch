d381b1d3a7bcbf8d3cf997d5f199e3e08ed1d933
Dominick Robinson <drobinson.lbe@gmail.com>
2025-02-25 13:04:20 -0500

    expanded team

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Dominick and Sid";
+    return "Dominick and Sid and Eyob and Itamar";
   }
 
   return "";
